62ba83a467a3695046e3718d5ad27f2a0a01ee0d
Joao Portela <joao.portela@gmail.com>
2018-11-04 01:21:18 +0000

    Initial config for Electron and Typescript

---
+++ webpack/webpack.config.js
@@ -0,0 +1,7 @@
+const mainConfig = require('./webpack.config.main');
+const playerConfig = require('./webpack.config.player');
+
+module.exports = [
+  mainConfig,
+  playerConfig,
+];
---
+++ webpack/webpack.config.player.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const baseConfig = require('./webpack.config.base');
+
+module.exports = Object.assign({}, baseConfig, {
+  target: 'electron-renderer',
+  entry: {
+    player: './src/player/index.ts',
+  },
+  plugins: [
+    new CopyWebpackPlugin([{
+      from: path.resolve(__dirname, '..', 'views', 'player.html'),
+      to: path.join(__dirname, '..', 'dist', 'player.html'),
+    }])
+  ]
+})
